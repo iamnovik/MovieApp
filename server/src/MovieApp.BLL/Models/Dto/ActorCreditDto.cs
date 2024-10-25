6472b1bb0ba5f7b75bb0b9f6585abc462dab565b
iamnovik <vlad09082003@mail.ru>
2024-10-25 15:08:28 +0300

    970866: Change ActorCreditDto to get Poster_Path

--- server/src/MovieApp.BLL/Models/Dto/ActorCreditDto.cs
+++ server/src/MovieApp.BLL/Models/Dto/ActorCreditDto.cs
@@ -5,4 +5,5 @@ public class ActorCreditDto
     public int Id { get; set; }             
     public string Character { get; set; } = null!;
     public string Title { get; set; } = null!;
+    public string Poster_Path { get; set; } = null!;
 }
